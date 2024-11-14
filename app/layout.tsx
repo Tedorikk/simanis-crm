f57f49943b3f522148b6b77a5b2ef410d34e4a7b
Tedrick <tedrikex@gmail.com>
2024-11-14 12:14:00 +0700

    Menambahkan dropdown pada avatar

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,14 @@ import "./globals.css";
 import { SidebarProvider, SidebarTrigger, SidebarInset } from "@/components/ui/sidebar"
 import { AppSidebar } from "@/components/app-sidebar"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -40,10 +48,20 @@ export default async function RootLayout({
           <SidebarInset>
             <header className="flex h-16 shrink-0 items-center justify-between border-b px-4">
               <SidebarTrigger className="-ml-1" />
-              <Avatar>
-                <AvatarImage src="https://github.com/shadcn.png" />
-                <AvatarFallback>ADM</AvatarFallback>
-              </Avatar>
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                <Avatar>
+                  <AvatarImage src="https://github.com/shadcn.png" />
+                  <AvatarFallback>ADM</AvatarFallback>
+                </Avatar>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>Profile</DropdownMenuItem>
+                  <DropdownMenuItem>Log out</DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
             </header>
             <main className="flex-1 overflow-auto">
               {children}
